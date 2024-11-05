33053bcd8da7d996b1ecff3d780b97ec42aa3449
lwj10024 <liangwenju10086@outlook.com>
2024-11-05 20:39:19 +0800

    相对地址访问图片

--- 4.10test.cpp
+++ 4.10test.cpp
@@ -8,8 +8,8 @@ using namespace std;
 int main()
 {
 	//将需要放映出来的图片文件放在项目当前目录下
-	Mat img = imread("C:\\Users\\LAPTOP2021\\Desktop\\1.png");
-	namedWindow("test");
+	Mat img = imread("img\\cat1.bmp");
+	namedWindow("test", WINDOW_NORMAL);
 	imshow("test", img);
 	waitKey(0);
 	return 0;
