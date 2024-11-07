9fa5b08a3b5d9d1e9bc874dd2b75a083f9803776
lwj10024 <liangwenju10086@outlook.com>
2024-11-07 12:57:29 +0800

    Add new project files

---
+++ opencv_demo/opencv_demo/main.cpp
@@ -0,0 +1,16 @@
+#include <opencv2/core/core.hpp>
+#include <opencv2/imgproc.hpp>
+#include <opencv2/highgui/highgui.hpp>
+#include <iostream>
+
+using namespace cv;
+using namespace std;
+int main()
+{
+	//将需要放映出来的图片文件放在项目当前目录下
+	Mat img = imread("../resources/images/cat1.bmp");
+	namedWindow("test", WINDOW_NORMAL);
+	imshow("test", img);
+	waitKey(0);
+	return 0;
+}
